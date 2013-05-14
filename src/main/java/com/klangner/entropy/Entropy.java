258c84d3e9f3c911898a3eee42b072a7550507a1
Krzysztof Langner <klangner@gmail.com>
2013-05-14 16:17:03 +0200

    dataset outside src

--- src/main/java/com/klangner/entropy/Entropy.java
+++ src/main/java/com/klangner/entropy/Entropy.java
@@ -1,9 +1,18 @@
 package com.klangner.entropy;
 
+import com.klangner.ast.INode;
+import com.klangner.ast.IParser;
+import com.klangner.ast.java.JavaASTParser;
+
 
 public class Entropy {
 	
-	public float calculateFileEntropy(String path){
-		return 0;
+	public double calculateFileEntropy(String filePath){
+		double score = 0;
+		IParser parser = new JavaASTParser();
+		INode rootNode = parser.parseFile(filePath);
+		int childCount = rootNode.getChildCount();
+		score = Math.log(childCount)/Math.log(2);
+		return score;
 	}
 }
--- src/test/java/com/klangner/entropy/EntropyTest.java
+++ src/test/java/com/klangner/entropy/EntropyTest.java
@@ -6,20 +6,20 @@ import org.junit.Test;
 
 public class EntropyTest {
 
-	private static String DATASET_PATH = "src/test/datasets/";
+	private static String DATASET_PATH = "datasets/";
 	
 	
 	@Test
 	public void entropy0() {
 		Entropy entropy = new Entropy();
-		float value = entropy.calculateFileEntropy(DATASET_PATH+"project1/Entropy0.java");
+		double value = entropy.calculateFileEntropy(DATASET_PATH+"project1/Entropy0.java");
 		assertEquals(0, value, 0.01);
 	}
 
 	@Test
 	public void entropy1() {
 		Entropy entropy = new Entropy();
-		float value = entropy.calculateFileEntropy(DATASET_PATH+"project1/Entropy1.java");
+		double value = entropy.calculateFileEntropy(DATASET_PATH+"project1/Entropy1.java");
 		assertEquals(1, value, 0.1);
 	}
 
