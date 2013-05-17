25758da58f5061f7e6858b50c189ebaf33767dfc
Krzysztof Langner <klangner@gmail.com>
2013-05-17 11:47:41 +0200

    Basic application working.

--- src/main/java/com/klangner/cantor/CantorApp.java
+++ src/main/java/com/klangner/cantor/CantorApp.java
@@ -1,5 +1,14 @@
 package com.klangner.cantor;
 
+import java.util.List;
+
+import com.klangner.ast.IModule;
+import com.klangner.ast.INode;
+import com.klangner.ast.IPackage;
+import com.klangner.ast.IParser;
+import com.klangner.ast.java.JavaASTParser;
+import com.klangner.cantor.modularity.DependencyAnalyzer;
+
 public class CantorApp {
 
 	public static void main(String[] args) {
@@ -21,8 +30,41 @@ public class CantorApp {
 	}
 
 	private void run(String command, String srcPath) {
-		System.out.println("Command: " + command);
-		System.out.println("src: " + srcPath);
+		
+		if(command.equals("list_modules")){
+			listModules(srcPath);
+		}
+		else if(command.equals("list_packages")){
+			listPackages(srcPath);
+		}
+		else{
+			printHelp();
+		}
+	}
+
+	private void listModules(String srcPath) {
+		DependencyAnalyzer analyzer = getDependencyAnalyzer(srcPath);
+		List<IModule> modules = analyzer.getModules();
+		System.out.println("Modules:");
+		for(IModule module: modules){
+			System.out.println(module.getName());
+		}
+	}
+
+	private DependencyAnalyzer getDependencyAnalyzer(String srcPath) {
+		IParser parser = new JavaASTParser();
+		INode ast = parser.parseProject(srcPath);
+		DependencyAnalyzer analyzer = new DependencyAnalyzer(ast);
+		return analyzer;
+	}
+
+	private void listPackages(String srcPath) {
+		DependencyAnalyzer analyzer = getDependencyAnalyzer(srcPath);
+		List<IPackage> packages = analyzer.getPackages();
+		System.out.println("Packages:");
+		for(IPackage p : packages){
+			System.out.println(p.getName());
+		}
 	}
 
 }
